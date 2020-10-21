beb0f300edfd6a085e1da67c8d5aa8e04c9bbf4c
maxxi2u1 <67334766+maxxi2u1@users.noreply.github.com>
2020-10-21 13:05:47 +0300

    Add files via upload

---
+++ Project.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Data Description
+
+# 1. The data was collected by Seattle Government. The data is classified according to the "SEVERITYCODE".
+# 2. The data consists of 194674 rows and 38 columns.
+# 3. We can further explore the data using other columns data which can will be significant in further classifying the accident cause.
+# 4. We could further classify the data for example using columns mentioned below:
+
+# In[ ]:
+
+
+
+
